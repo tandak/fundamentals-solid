cb685f8e9a1de3ac11bf8eaf4c36970d7b0d80e5
Tanda <tanda.kabanda@asos.com>
2020-10-27 22:35:16 +0000

    Interface Segregation Principle

---
+++ Src/Solid.Lib/InterfaceSegregationPrinciple.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Solid.Lib
+{
+    public class InterfaceSegregationPrinciple
+    {
+        public string Principle = " No client should be forced to implement methods which it does not use";
+    }
+
+    public interface IOrder
+    {
+        void AddToCart();
+    }
+
+    public interface IOnlineOrder
+    {
+        void CCProcess();
+    }
+
+    public class OnlineOrder : IOrder, IOnlineOrder
+    {
+        public void AddToCart()
+        {
+            //Do Add to Cart
+        }
+
+        public void CCProcess()
+        {
+            //process through credit card
+        }
+    }
+
+    public class OfflineOrder : IOrder
+    {
+        public void AddToCart()
+        {
+            //Do Add to Cart
+        }
+    }
+}
